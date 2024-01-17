3808fb461843f479389c6a11c4aab627ed3cfd2a
soonhankwon <soonable@naver.com>
2024-01-17 09:12:56 +0900

    components skelecton

---
+++ src/components/PostEditForm.svelte
@@ -0,0 +1,26 @@
+<!-- do-u-understand-content-edit-form start-->
+<div class="do-u-understand-content-box">
+  <div class="content-box-header">
+    <div class="content-box-header-inner-left">
+      <p class="p-user">freeseamew</p>
+      <p class="p-date">2022-11-11</p>
+    </div>
+  </div>
+
+  <div class="content-box-main">
+    <textarea
+      id="message"
+      rows="5"
+      class="do-u-understand-content-textarea"
+      placeholder="내용을 입력해 주세요."
+    ></textarea>
+  </div>
+
+  <div class="content-box-bottom">
+    <div class="button-box">
+      <button class="button-base">완료</button>
+      <button class="button-base">취소</button>
+    </div>
+  </div>
+</div>
+<!-- do-u-understand-content-edit-form end-->
---
+++ src/components/PostList.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Post from "./Post.svelte";
+  import PostLoading from "./PostLoading.svelte";
+</script>
+
+<!-- do-u-understand-list-wrap start-->
+<div class="do-u-understand-list-wrap">
+  <ul class="do-u-understand-ul">
+    <li class="mb-5">
+      <Post />
+    </li>
+  </ul>
+</div>
+<!-- do-u-understand-list-wrap end-->
