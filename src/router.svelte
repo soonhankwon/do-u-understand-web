962fcb650dcc43de91f47367f05157dc92b5912b
soonhankwon <soonable@naver.com>
2024-01-17 09:25:37 +0900

    feat: router

---
+++ src/router.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Route } from "tinro";
+
+  import Posts from "./pages/Posts.svelte";
+  import Login from "./pages/Login.svelte";
+  import Register from "./pages/Register.svelte";
+</script>
+
+<Route path="/" redirect="/posts" />
+<Route paht="/posts/*"><Posts /></Route>
+<Route paht="/login/*"><Login /></Route>
+<Route paht="/register/*"><Register /></Route>
