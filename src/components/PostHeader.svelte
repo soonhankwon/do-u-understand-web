a2195fe511ef8fb057579d1597386ffa937a587f
soonhankwon <soonable@naver.com>
2024-01-25 13:05:09 +0900

    feat: update icon iconify

--- src/components/PostHeader.svelte
+++ src/components/PostHeader.svelte
@@ -2,10 +2,14 @@
   import { router } from "tinro";
   import { auth, isLogin, postsMode } from "../stores";
   import { ALL, SUBSCRIBE, MY, WRITE } from "../utils/constant";
+  import Icon from "@iconify/svelte";
 
   const goLogin = () => router.goto("/login");
   const onLogout = () => auth.logout();
-  const onEditUserInfo = () => router.goto("/userInfo");
+  const onEditUserInfo = () => {
+    postsMode.changeMode();
+    router.goto("/userInfo");
+  };
 
   const onChangeMode = (mode) => {
     // if ($postsMode !== mode) postsMode.changeMode(mode);
@@ -20,28 +24,42 @@
     <button
       class="main-menu mr-6"
       class:main-menu-selected={$postsMode === ALL}
-      on:click={() => onChangeMode(ALL)}>NOTICE</button
+      on:click={() => onChangeMode(ALL)}
+      ><Icon
+        icon="icon-park-twotone:volume-notice"
+        color="white"
+        width="24"
+      /></button
     >
     {#if $isLogin}
       <button
         class="main-menu mr-6"
         class:main-menu-selected={$postsMode === SUBSCRIBE}
-        on:click={() => onChangeMode(SUBSCRIBE)}>SUBSCRIBE</button
+        on:click={() => onChangeMode(SUBSCRIBE)}
+        ><Icon icon="solar:mailbox-bold" color="white" width="24" /></button
       >
       <button
         class="main-menu mr-6"
         class:main-menu-selected={$postsMode === MY}
-        on:click={() => onChangeMode(MY)}>MYPOST</button
+        on:click={() => onChangeMode(MY)}
+        ><Icon icon="gg:list" color="white" width="24" /></button
       >
       <button
         class="main-menu"
         class:main-menu-selected={$postsMode === WRITE}
-        on:click={() => onChangeMode(WRITE)}>WRITE</button
+        on:click={() => onChangeMode(WRITE)}
+        ><Icon icon="ic:baseline-plus" color="white" width="24" /></button
       >
     {:else}
-      <button class="main-menu mr-6 main-menu-blocked">SUBSCRIBE</button>
-      <button class="main-menu mr-6 main-menu-blocked">MY POST</button>
-      <button class="main-menu main-menu-blocked">WRITE</button>
+      <button class="main-menu mr-6 main-menu-blocked"
+        ><Icon icon="solar:mailbox-bold" color="white" width="24" /></button
+      >
+      <button class="main-menu mr-6 main-menu-blocked"
+        ><Icon icon="gg:list" color="white" width="24" /></button
+      >
+      <button class="main-menu main-menu-blocked"
+        ><Icon icon="ic:baseline-plus" color="white" width="24" /></button
+      >
     {/if}
   </nav>
 
@@ -50,26 +68,12 @@
     <button href="#" class="text-white" on:click={onEditUserInfo}>
       <p>{$auth.email}</p>
     </button>
-    <button href="#" class="text-white" on:click={onLogout}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        class="h-7 w-7 fill-white"
-        ><path d="m13 16 5-4-5-4v3H4v2h9z"></path><path
-          d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"
-        ></path></svg
-      >
+    <button href="#" on:click={onLogout}>
+      <Icon icon="material-symbols:logout" color="white" width="24" />
     </button>
   {:else}
-    <button href="#" class="text-white" on:click={goLogin}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        class="h-7 w-7 fill-white"
-        ><path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path><path
-          d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"
-        ></path></svg
-      >
+    <button href="#" on:click={goLogin}>
+      <Icon icon="material-symbols:login" color="white" width="24" />
     </button>
   {/if}
 </header>
