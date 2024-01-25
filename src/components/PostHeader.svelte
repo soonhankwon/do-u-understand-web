c7d12ea7fb0b70536cae67bc9dbb024f761d7943
soonhankwon <soonable@naver.com>
2024-01-25 14:15:40 +0900

    feat: icon & added title header element

--- src/components/PostHeader.svelte
+++ src/components/PostHeader.svelte
@@ -19,9 +19,10 @@
 
 <!-- start header -->
 <header class="main-header">
-  <p class="p-main-title">DO-U-UNDERSTAND</p>
+  <p class="p-main-title">SELFnews</p>
   <nav class="main-nav">
     <button
+      title="Notification"
       class="main-menu mr-6"
       class:main-menu-selected={$postsMode === ALL}
       on:click={() => onChangeMode(ALL)}
@@ -33,31 +34,37 @@
     >
     {#if $isLogin}
       <button
+        title="Subscribe"
         class="main-menu mr-6"
         class:main-menu-selected={$postsMode === SUBSCRIBE}
         on:click={() => onChangeMode(SUBSCRIBE)}
         ><Icon icon="solar:mailbox-bold" color="white" width="24" /></button
       >
       <button
+        title="MyPost"
         class="main-menu mr-6"
         class:main-menu-selected={$postsMode === MY}
         on:click={() => onChangeMode(MY)}
         ><Icon icon="gg:list" color="white" width="24" /></button
       >
       <button
-        class="main-menu"
+        title="Create"
+        class="main-menu mr-6"
         class:main-menu-selected={$postsMode === WRITE}
         on:click={() => onChangeMode(WRITE)}
         ><Icon icon="ic:baseline-plus" color="white" width="24" /></button
       >
+      <button title="MyInfo" href="#" on:click={onEditUserInfo}>
+        <Icon icon="ic:baseline-account-box" color="white" width="24" />
+      </button>
     {:else}
-      <button class="main-menu mr-6 main-menu-blocked"
+      <button title="Subscribe" class="main-menu mr-6 main-menu-blocked"
         ><Icon icon="solar:mailbox-bold" color="white" width="24" /></button
       >
-      <button class="main-menu mr-6 main-menu-blocked"
+      <button title="MyPost" class="main-menu mr-6 main-menu-blocked"
         ><Icon icon="gg:list" color="white" width="24" /></button
       >
-      <button class="main-menu main-menu-blocked"
+      <button title="Create" class="main-menu main-menu-blocked"
         ><Icon icon="ic:baseline-plus" color="white" width="24" /></button
       >
     {/if}
@@ -65,14 +72,11 @@
 
   <!--로그인 -->
   {#if $isLogin}
-    <button href="#" class="text-white" on:click={onEditUserInfo}>
-      <p>{$auth.email}</p>
-    </button>
-    <button href="#" on:click={onLogout}>
+    <button title="Logout" href="#" on:click={onLogout}>
       <Icon icon="material-symbols:logout" color="white" width="24" />
     </button>
   {:else}
-    <button href="#" on:click={goLogin}>
+    <button title="Login" href="#" on:click={goLogin}>
       <Icon icon="material-symbols:login" color="white" width="24" />
     </button>
   {/if}
