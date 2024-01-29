2674c70b4c4ee19fa3c6926cc02ca064ec9be55c
soonhankwon <soonable@naver.com>
2024-01-29 10:36:19 +0900

    feat: update noti

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -1,41 +1,72 @@
-<div class="news-magazine-container">
-  <div class="news-magazine-header">
+<div class="news-container">
+  <div class="news-header">
     <h1>SELFnews HeadLine</h1>
   </div>
-  <div class="news-magazine-content-main">
+  <div class="news-content-main">
     <p>
-      <span class="highlight-text">SELFnews</span>에 오신 것을 환영합니다.
-      "공부한 것을 왜 자꾸 까먹는 걸까?"라고 생각하거나 리뷰 습관에 대해 의문을
-      품은 적이 있나요?
+      <span class="highlight-text">SELFnews</span>에 오신 것을 환영합니다!
+      <br />
+      SELFnews는 메모앱에서 수 많은 공부 메모를 작성하지만, 정작 리뷰는 귀찮아 공부한
+      내용을 까먹는 문제를 해결하고자 만든 시스템입니다.
     </p>
 
     <ul>
       <li>
-        메모앱에서 수 많은 공부 메모를 작성하지만, 정작 리뷰는 귀찮아 까먹는
-        저의 문제를 해결하고자 만든 시스템입니다.
+        <span class="highlight-text">SELFnews</span>의 핵심 기능은 2가지 입니다.
       </li>
+      <li>첫째, 포스트 작성, 발행, 구독 기능</li>
+      <li>두번째, 구독 포스트 정기적 이메일 발송 기능</li>
+      <br />
       <li>
-        정기적인 뉴스레터에서 아이디어를 가져온 <span class="highlight-text"
-          >SELFnews</span
-        >는 메타인지와 학습 개선을 위해 만들어진 무료 웹 애플리케이션입니다.
-        자신이 공부한 자료를 간단히 요약, 발행, 구독을 통해 이메일을 통해
-        주기적인 알림을 받아보세요.
+        결론부터 말하자면, 자신이 등록한 포스트를 주기적인 이메일 알람을 통해
+        리마인드 시키는 것으로 지식을 까먹지 않도록 합니다.
       </li>
+      <br />
       <li>
-        포스트는 심플하게 작성하는 것을 추천합니다. 이메일 알람을 통해 자신이
-        작성한 간단한 정리를 확인하고, 참조링크를 통해 자세하게 내용을 공부하고
-        업데이트하는식으로 활용하고 있습니다.
+        정기적인 뉴스레터에서 아이디어를 가져온 SELFnews는 자신이 작성하고
+        선택적으로 구독한 포스트를 아침, 점심, 저녁으로 자신의 이메일로 발송 및
+        수신하도록 만들어 리마인드를 가능하게 합니다.
       </li>
+      <br />
+
       <li>
-        개인정보는 모두 암호화되어있으며, 서비스는 지속해서 업데이트 예정입니다.
+        예를 들어 인터넷 강의를 듣고, 메모앱에 공부한 내용을 SELFnews에 간단하게
+        포스팅합니다.
+        <br />
+        포스트에는 제목, 간단한 정리 내용, 링크를 첨부해서 발행합니다. 발행한 포스트를
+        구독하고 싶다면 구독버튼을 누릅니다.
+        <br />
+        <br />
+        구독한 포스트는 내부적인 알고리즘에 따라 08:00, 13:00, 20:00 이메일 발송됩니다.
+        <br />
+        <br />
+        이메일 알람이 모바일에 설정되어있다면, 효과적으로 알람을 확인할 수 있습니다.
+        이메일을 확인하고 좀 더 자세한 내용을 리마인드 하고 싶다면, 참조 링크를 통해
+        학습할 수 있습니다. 특정 포스트가 더 이상 구독할 필요가 없다고 생각하면 구독을
+        해제하거나 내용을 업데이트 시킵니다.
       </li>
-      <li>해당 애플리케이션은 웹에 최적화되있습니다.</li>
+      <br />
+      <li>
+        심플한 시스템이지만 실제로 사용한 결과 공부한 내용의 리뷰를 강제하는
+        효과가 있어 공부하고 까먹는 문제를 50%이상 개선시킬 수 있었습니다.
+      </li>
+      <br />
+      <li>
+        SELFnews를 알람 시스템을 통해 자신이 꼭 기억하고 싶은 내용들을 진짜
+        자신의 지식으로 만들어보세요.
+      </li>
+      <br />
+      <li>
+        * 개인정보는 모두 암호화되어있으며, 서비스는 지속해서 업데이트
+        예정입니다.
+      </li>
+      <li>* 해당 애플리케이션은 웹에 최적화되있습니다.</li>
     </ul>
   </div>
 </div>
 
 <style>
-  .news-magazine-container {
+  .news-container {
     max-width: 800px;
     margin: 20px auto;
     background-color: #f8f8f8;
@@ -45,7 +76,7 @@
     box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
   }
 
-  .news-magazine-header {
+  .news-header {
     background-color: #3498db;
     color: #fff;
     padding: 20px;
@@ -54,7 +85,7 @@
     font-weight: bold;
   }
 
-  .news-magazine-content-main {
+  .news-content-main {
     padding: 20px;
     font-size: 1.2rem;
     line-height: 1.6;
@@ -66,17 +97,17 @@
     font-weight: bold;
   }
 
-  .news-magazine-content-main p {
+  .news-content-main p {
     margin: 0 0 20px 0;
   }
 
-  .news-magazine-content-main ul {
+  .news-content-main ul {
     list-style-type: none;
     padding: 0;
     margin: 0;
   }
 
-  .news-magazine-content-main li {
+  .news-content-main li {
     margin-bottom: 10px;
   }
 </style>
